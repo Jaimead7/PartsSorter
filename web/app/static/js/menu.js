a5e24ef7c86fef9a07ec7cebe9f757ea1460128b
Jaime Álvarez Díaz <alvarez.diaz.jaime1@gmail.com>
2025-10-03 14:49:42 +0200

    Working on style refactor

--- web/app/static/js/menu.js
+++ web/app/static/js/menu.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleBtn = document.getElementById("toggle-sidebar");
+    const toggleBtn = document.getElementById("menu-button");
     const sidebar = document.getElementById("sidebar");
-    const mainContent = document.getElementById("main-content");
 
     toggleBtn.addEventListener("click", () => {
         sidebar.classList.toggle("sidebar-collapsed");
-        mainContent.classList.toggle("main-content-expanded")
     });
 });
 
 function collapsSidebar() {
     const sidebar = document.getElementById("sidebar");
-    const mainContent = document.getElementById("main-content");
     if (window.innerWidth < 650) {
         sidebar.classList.add("sidebar-collapsed")
-        mainContent.classList.add("main-content-expanded")
     }
 }
 
