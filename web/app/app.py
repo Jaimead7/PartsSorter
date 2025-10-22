c55ccf38d036ecb11b5102253545c959f5a100bf
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2025-10-22 08:43:14 +0200

    Add chrome devtools endpoint

--- web/app/app.py
+++ web/app/app.py
@@ -1,26 +1,35 @@
 import uvicorn
-from quart import Quart, render_template
+from quart import Quart, render_template, render_template_string
 
 from .blueprints import api_bp, config_bp
-from .dependencies.config import SERVER_PORT, SERVER_IP
+from .dependencies.config import SERVER_IP, SERVER_PORT
 
 app = Quart(__name__)
 
 @app.route('/')
 @app.route('/index')
 async def home() -> str:
-    return await render_template('home.html')
+    return await render_template(
+        'index.html',
+        page_title= 'PART INSPECTION'
+    )
 
 @app.route('/image-stream')
 async def image_stream() -> str:
-    return await render_template('image-stream.html')
+    return await render_template(
+        'image-stream.html',
+        page_title= 'Airbag cut inspection - Image stream'
+    )
 
 app.register_blueprint(config_bp)
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
     uvicorn.run(
-        app,
+        'app.app:app',
         host= SERVER_IP,
-        port= SERVER_PORT
+        port= SERVER_PORT,
+        reload= True,
+        reload_dirs=['.', './templates', './static'],
+        reload_includes=['*.py', '*.html', '*.css', '*.js']
     )
---
+++ web/app/blueprints/well_known.py
@@ -0,0 +1,12 @@
+from quart import Blueprint, Response, jsonify
+
+wn_bp = Blueprint(
+    '.well-known',
+    __name__,
+    url_prefix='/.well-known'
+)
+
+@wn_bp.route('/appspecific/com.chrome.devtools.json')
+async def chrome_dev_tools() -> Response:
+    config: dict = {}
+    return jsonify(config)
