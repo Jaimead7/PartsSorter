1e323318ded7e86b028cc921b5af1b2b7d341880
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2025-10-31 11:40:31 +0100

    Add cookies for image-stream filter

--- web/app/static/js/filters/origin.js
+++ web/app/static/js/filters/origin.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // COLLAPSE
     const collapseElement = document.getElementById('originFilterCollapseCard');
     const buttonIcon = document.querySelector('button[data-bs-target="#originFilterCollapseCard"] .bi');
-    
+
     collapseElement.addEventListener('show.bs.collapse', function() {
         buttonIcon.classList.remove('bi-caret-down-square');
         buttonIcon.classList.add('bi-caret-up-square');
     });
-    
+
     collapseElement.addEventListener('hide.bs.collapse', function() {
         buttonIcon.classList.remove('bi-caret-up-square');
         buttonIcon.classList.add('bi-caret-down-square');
@@ -16,26 +16,56 @@ document.addEventListener('DOMContentLoaded', function() {
     // SELECT ALL
     const selectAllCheckbox = document.getElementById('originFilterSelectAll');
     const originOptions = document.querySelectorAll('input[name="origin-filter-option"]');
-    
-    function updateSelectAllState() {
-        const allChecked = Array.from(originOptions).every(checkbox => checkbox.checked);
-        const someChecked = Array.from(originOptions).some(checkbox => checkbox.checked);
-        
-        selectAllCheckbox.checked = allChecked;
-        selectAllCheckbox.indeterminate = someChecked && !allChecked;
-    }
-    
+
+    function saveOriginsOptions() {
+        const selectedOrigins = Array.from(originOptions)
+            .filter(checkbox => checkbox.checked)
+            .map(checkbox => checkbox.value);
+        const expirationDate = new Date();
+
+        expirationDate.setDate(expirationDate.getDate() + 30);
+        document.cookie = `originFilterOptions=${JSON.stringify(selectedOrigins)}; expires=${expirationDate.toUTCString()}; path=/`;
+    };
+
+    function loadOriginsOptions() {
+        const cookieValue = document.cookie
+            .split('; ')
+            .find(row => row.startsWith('originFilterOptions='));
+
+        if (cookieValue) {
+            const selectedOrigins = JSON.parse(cookieValue.split('=')[1]);
+            originOptions.forEach(checkbox => {
+                checkbox.checked = selectedOrigins.includes(checkbox.value);
+            });
+        }
+    };
+
     selectAllCheckbox.addEventListener('change', function() {
         const isChecked = this.checked;
+
         originOptions.forEach(checkbox => {
             checkbox.checked = isChecked;
         });
+
         selectAllCheckbox.indeterminate = false;
+
+        saveOriginsOptions();
     });
+
+    function updateSelectAllState() {
+        const allChecked = Array.from(originOptions).every(checkbox => checkbox.checked);
+        const someChecked = Array.from(originOptions).some(checkbox => checkbox.checked);
+
+        selectAllCheckbox.checked = allChecked;
+        selectAllCheckbox.indeterminate = someChecked && !allChecked;
+
+        saveOriginsOptions();
+    };
 
     originOptions.forEach(checkbox => {
         checkbox.addEventListener('change', updateSelectAllState);
     });
 
+    loadOriginsOptions();
     updateSelectAllState();
 });
