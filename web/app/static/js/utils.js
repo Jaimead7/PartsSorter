39c6cc75074d41e8ead95c244c21cd6d120412af
Jaime Alvarez <alvarez.diaz.jaime1@gmail.com>
2025-11-25 11:45:11 +0100

    Move alert to center

--- web/app/static/js/utils.js
+++ web/app/static/js/utils.js
@@ -2,7 +2,7 @@ let apiIP = null;
 
 async function showAlert(message, type, seconds) {
     const alertBlock = document.createElement("div");
-    alertBlock.className = `alert alert-${type} fade show position-absolute m-1`;
+    alertBlock.className = `alert alert-${type} fade show position-absolute m-1 start-50 translate-middle-x text-truncate`;
     alertBlock.style.cssText = 'top:4rem; z-index: 9999;';
     alertBlock.textContent = message;
     document.getElementById("main-content").appendChild(alertBlock);
