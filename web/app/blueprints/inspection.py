c3a18614015e9995203c51b23a0bf7081bb7ed36
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2026-01-09 11:54:46 +0100

    Typing for model responses

--- web/app/blueprints/inspection.py
+++ web/app/blueprints/inspection.py
@@ -26,6 +26,8 @@ from werkzeug import Response
 from ..dependencies.api import (api_get_image_extensions,
                                 api_get_inspection_results, api_get_models,
                                 api_get_origins)
+from ..models.api import (InspectionResultResponse, ModelResponse,
+                          OriginResponse)
 
 inspection_bp = Blueprint(
     'inspection',
@@ -36,7 +38,7 @@ inspection_bp = Blueprint(
 @inspection_bp.route('/image-stream')
 async def image_stream() -> str:
     try:
-        origins: list[str] = await api_get_origins()
+        origins: list[OriginResponse] = await api_get_origins()
     except httpx.ConnectError:
         origins = []
     preselected_origins: list[str] = request.args.getlist('origin')
@@ -50,9 +52,9 @@ async def image_stream() -> str:
 @inspection_bp.route('/image-inspection')
 async def image_inspection() -> Response | str:
     try:
-        origins: list[str] = await api_get_origins()
-        models: list[str] = await api_get_models()
-        classes: list[str] = await api_get_inspection_results()
+        origins: list[OriginResponse] = await api_get_origins()
+        models: list[ModelResponse] = await api_get_models()
+        classes: list[InspectionResultResponse] = await api_get_inspection_results()
         image_extensions: list[str] = await api_get_image_extensions()
     except httpx.ConnectError:
         return redirect('/', 302)
