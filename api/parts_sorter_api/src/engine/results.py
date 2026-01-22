70d5b140148fe7da7e765f90f44313e73ca2e973
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2026-01-22 16:32:33 +0100

    fix extract_first_result

--- api/parts_sorter_api/src/engine/results.py
+++ api/parts_sorter_api/src/engine/results.py
@@ -151,5 +151,6 @@ def extract_first_result(results: ResutlsType) -> tuple[Optional[int], Optional[
     results_array: np.ndarray = results.boxes.data
     if len(results_array) == 0:
         return (None, None)
+    first: np.ndarray = results_array[0]
     # [x0, y0, x1, y1, conf, id] x n
-    return (int(results_array[-1]), float(results_array[-2]))
+    return (int(first[-1]), float(first[-2]))
