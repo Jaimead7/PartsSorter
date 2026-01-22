4306a2dbf1e7d61e1764d1f189d5f1875b8dfcd0
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2026-01-22 11:42:10 +0100

    Create results sorters structure

---
+++ api/parts_sorter_api/src/engine/results_sorters.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2025 Jaime Álvarez Díaz <alvarez.diaz.jaime1@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+from typing import Protocol
+
+from .results import ResutlsType
+
+
+class ResultsSorterFunction(Protocol):
+    def __call__(self, results: ResutlsType) -> ResutlsType:
+        ...
+
+
+def no_sort(results: ResutlsType) -> ResutlsType:
+    return results
+
+
+RESULTS_SORTERS: dict[str, ResultsSorterFunction] = {
+    'NONE': no_sort
+}
+
+def results_sorter_factory(name: str) -> ResultsSorterFunction:
+    try:
+        return RESULTS_SORTERS[name.upper()]
+    except KeyError:
+        return no_sort
